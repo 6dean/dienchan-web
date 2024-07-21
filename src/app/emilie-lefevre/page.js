3ec4f07a2da4fdf91f8442b50bc45e39b850ccc6
6dean <ferdjallahrockdean@gmail.com>
2024-07-21 18:03:27 +0200

    update

--- src/app/emilie-lefevre/page.js
+++ src/app/emilie-lefevre/page.js
@@ -1,7 +1,65 @@
 export default function Experiences() {
   return (
     <main>
-      <div>Emilie Lefevre</div>
+      <div className="head-bio">Jeunesse et Formation</div>
+
+      <p className="text-bio">
+        Émilie Lefevre est née et a grandi à Lyon, une ville connue pour sa
+        riche histoire culturelle et gastronomique. Dès son plus jeune âge,
+        Émilie a montré un intérêt marqué pour les médecines alternatives et les
+        pratiques de bien-être. Elle passait des heures à lire des livres sur
+        l'acupuncture, la phytothérapie et d'autres médecines traditionnelles,
+        souvent accompagnée de sa grand-mère, une herboriste passionnée. Après
+        avoir terminé ses études secondaires, Émilie a entrepris un cursus
+        universitaire en biologie à l'Université Claude Bernard Lyon 1. Durant
+        ses études, elle a découvert le Dien Chan, une technique vietnamienne de
+        réflexologie faciale, lors d'un séminaire sur les médecines
+        traditionnelles asiatiques.
+      </p>
+
+      <div className="head-bio">Découverte du Dien Chan</div>
+
+      <p className="text-bio">
+        Émilie a été immédiatement fascinée par le Dien Chan, une méthode qui
+        combine réflexologie et stimulation de points spécifiques sur le visage
+        pour traiter divers maux et améliorer la santé globale. Son intérêt pour
+        cette discipline l'a conduite à effectuer un voyage au Vietnam pour
+        étudier directement auprès des maîtres de la discipline. Elle a passé
+        deux années à Hanoï, où elle a suivi une formation intensive sous la
+        direction de Professeur Bùi Quôc Châu, le fondateur de la méthode Dien
+        Chan. Pendant cette période, elle a non seulement appris les techniques
+        de stimulation des points réflexes, mais aussi les bases de la
+        philosophie et de la médecine traditionnelle vietnamienne.
+      </p>
+
+      <div className="head-bio">Carrière</div>
+
+      <p className="text-bio">
+        De retour en France en 2010, Émilie a ouvert son propre cabinet de Dien
+        Chan à Lyon. Sa pratique a rapidement gagné en popularité, grâce à ses
+        compétences exceptionnelles et à sa capacité à adapter la méthode pour
+        répondre aux besoins spécifiques de ses patients. Elle a développé une
+        approche holistique, intégrant le Dien Chan avec d'autres techniques de
+        bien-être telles que l'aromathérapie et le massage thérapeutique. En
+        plus de son cabinet, Émilie Lefevre a commencé à donner des conférences
+        et des ateliers à travers la France, partageant ses connaissances et
+        formant de nouveaux praticiens en Dien Chan. Elle a également publié
+        plusieurs ouvrages sur la réflexologie faciale, traduisant des textes
+        vietnamiens originaux et y ajoutant ses propres observations et
+        expériences.
+      </p>
+
+      <div className="head-bio">Influence et Héritage</div>
+
+      <p className="text-bio">
+        Émilie Lefevre est reconnue comme une pionnière du Dien Chan en Europe.
+        Son travail a non seulement aidé de nombreuses personnes à améliorer
+        leur santé et leur bien-être, mais a également contribué à faire
+        connaître cette méthode traditionnelle vietnamienne au grand public
+        occidental. Elle continue d'innover et de chercher de nouvelles façons
+        d'intégrer les pratiques de guérison ancestrales dans la vie moderne,
+        inspirant une nouvelle génération de praticiens de Dien Chan.
+      </p>
     </main>
   );
 }
--- src/app/tarifs/page.js
+++ src/app/tarifs/page.js
@@ -1,7 +1,69 @@
 export default function Tarifs() {
+  const pricing = [
+    { session: "Consultation Initiale", duration: "1 heure", price: "70€" },
+    { session: "Séance de Suivi", duration: "45 minutes", price: "50€" },
+    {
+      session: "Forfait Bien-être (5 séances)",
+      duration: "45 minutes/séance",
+      price: "220€",
+    },
+    { session: "Séance Express", duration: "30 minutes", price: "35€" },
+    { session: "Massage du Visage", duration: "30 minutes", price: "40€" },
+  ];
+
   return (
-    <main>
-      <div>Les tarifs</div>
-    </main>
+    <div>
+      <div style={styles.container}>
+        <h1 style={styles.header}>Tarifs des Séances de Dien Chan</h1>
+        <table style={styles.table}>
+          <thead>
+            <tr>
+              <th style={styles.th}>Type de Séance</th>
+              <th style={styles.th}>Durée</th>
+              <th style={styles.th}>Tarif</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pricing.map((item, index) => (
+              <tr key={index} style={styles.tr}>
+                <td style={styles.td}>{item.session}</td>
+                <td style={styles.td}>{item.duration}</td>
+                <td style={styles.td}>{item.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div>Type de paiement accepté</div>
+      <div>CB, Paypal, etc</div>
+      <div>Prendre Rendez-vous</div>
+    </div>
   );
 }
+
+const styles = {
+  container: {
+    maxWidth: "1200px",
+    margin: "0 auto",
+    padding: "20px",
+    fontFamily: "Fustat, sans-serif",
+  },
+  header: {
+    fontSize: "24px",
+    marginBottom: "20px",
+  },
+  table: { backgroundColor: "grey", width: "100%", borderCollapse: "collapse" },
+  th: {
+    borderBottom: "2px solid black",
+    textAlign: "left",
+    padding: "12px",
+    backgroundColor: "blue",
+  },
+  tr: {
+    borderBottom: "1px solid #ddd",
+  },
+  td: {
+    padding: "15px",
+    fontSize: "18px",
+  },
+};
